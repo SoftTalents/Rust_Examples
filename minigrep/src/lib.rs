1b286c6d653328018bdd07e7d138412d5a69b443
chrisbrights <softtalents1986@outlook.com>
2022-12-29 06:02:41 +0900

    simplify search function by using iterator adaptor

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -23,7 +23,7 @@ impl Config {
             Some(arg) => arg,
             None => return Err("Didn't get a file path"),
         };
-        
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
             query,
@@ -49,13 +49,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
