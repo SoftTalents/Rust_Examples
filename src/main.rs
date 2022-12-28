11f9fed37d7609dfccb7522c48e868ff81962fa8
cryptohero92 <alwaysflexibleman@gmail.com>
2022-12-28 10:09:43 +0900

    returning Result instead of panic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text:\n{contents}");
@@ -15,16 +19,16 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config {
+        Ok(Config {
             query,
             file_path
-        }
+        })
     }
 }
 
