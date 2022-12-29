fcf446e5573a248553c1b7e50613ebc1be714df1
cryptohero92 <alwaysflexibleman@gmail.com>
2022-12-29 15:52:31 +0900

    Deref trait and coercion work

--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,6 +1,34 @@
 use add_one;
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}");
+}
 
 fn main() {
     let num = 10;
     println!("Hello, world! {num} plus one is {}!", add_one::add_one(num));
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, *y);
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
