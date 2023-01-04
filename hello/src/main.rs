5dd3557973bda8d5a39f5fa1758854248b4465c7
cryptohero92 <alwaysflexibleman@gmail.com>
2023-01-04 16:40:50 +0900

    take only 2 request

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -11,12 +11,14 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -48,5 +50,6 @@ fn handle_connection(mut stream: TcpStream) {
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
     // println!("Request: {:#?}", http_request);
 }
